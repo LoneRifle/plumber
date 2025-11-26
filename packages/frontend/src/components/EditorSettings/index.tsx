21cace8f5ae157a4295cb056950990f03f383ae9
kevinkim-ogp <kevinkim@open.gov.sg>
2025-11-26 21:19:17 +0800

    chore: only show if has collaborators

--- packages/frontend/src/components/EditorSettings/index.tsx
+++ packages/frontend/src/components/EditorSettings/index.tsx
@@ -56,6 +56,8 @@ export default function EditorSettingsLayout(
     variables: { id: flowId },
   })
   const flow: IFlow = data?.getFlow
+  const hasCollaborators =
+    flow?.collaborators?.length && flow.collaborators.length > 1 // includes owner
 
   const [isDrawerOpen, setDrawerOpen] = useState(false)
 
@@ -79,17 +81,18 @@ export default function EditorSettingsLayout(
             },
           ].filter(Boolean),
         },
-        showCollaborators && {
-          group: 'Manage Pipe',
-          links: [
-            {
-              Icon: BiLink,
-              text: 'Connections',
-              to: URLS.FLOW_EDITOR_CONNECTIONS(flowId),
-              group: 'Manage Pipe' as const,
-            },
-          ],
-        },
+        showCollaborators &&
+          hasCollaborators && {
+            group: 'Manage Pipe',
+            links: [
+              {
+                Icon: BiLink,
+                text: 'Connections',
+                to: URLS.FLOW_EDITOR_CONNECTIONS(flowId),
+                group: 'Manage Pipe' as const,
+              },
+            ],
+          },
         {
           group: 'Notifications',
           links: [
@@ -105,7 +108,7 @@ export default function EditorSettingsLayout(
       () => setDrawerOpen(true),
       () => setDrawerOpen(false),
     ],
-    [flowId, setDrawerOpen, showCollaborators],
+    [flowId, setDrawerOpen, showCollaborators, hasCollaborators],
   )
 
   const drawerComponent = useBreakpointValue({
